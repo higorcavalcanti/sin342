ba53555964b521f8a5496320a8db8378b469babf
Higor Daniel <higordaniell@live.com>
2016-11-24 01:37:07 -0200

    Atendimento

--- app/Views/atendimento/index.php
+++ app/Views/atendimento/index.php
@@ -1,4 +1,4 @@
-<h1>Atendimento Index</h1>
+<h1>Atendimento - Perguntas frequentes</h1>
 <?php foreach($this->atendimentos as $atendimento): ?>
     <h2><?= $atendimento->getPergunta(); ?></h2>
     <h3><?= $atendimento->getResposta(); ?></h3>
