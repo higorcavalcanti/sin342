0824058e4cfea87f4a4accf3a27d8776b86137d4
Daniel Araujo <danielaraujos@live.com>
2016-11-24 20:58:47 -0200

    Arrumando login

--- app/Views/auth/index.php
+++ app/Views/auth/index.php
@@ -5,12 +5,12 @@
             <h2 class="center">Já tem conta?</h2>
 
             <form method="post">
-                <input type="hidden" name="action" value="login">
+                <input type="hidden" name="action" value="login"><br>
 
-                <label for="email">Email:</label>
-                <input type="email" name="email" placeholder="Digite seu e-mail" required>
+                <label for="email">Email:</label><br>
+                <input type="email" name="email" placeholder="Digite seu e-mail" required><br>
 
-                <label for="senha">Senha:</label>
+                <label for="senha">Senha:</label><br>
                 <input type="password" name="senha" placeholder="Digite sua senha" required>
 
                 <div class="alertError"><?php echo $this->erro['login'];?></div>
@@ -23,16 +23,16 @@
         <div class="auth" id="registro">
             <h2 class="center">Ainda não tem conta?</h2>
             <form method="post">
-                <input type="hidden" name="action" value="registro">
+                <input type="hidden" name="action" value="registro"><br>
 
-                <label for="email">Nome completo:</label>
-                <input type="text" name="nome" placeholder="Digite seu nome completo">
+                <label for="email">Nome completo:</label><br>
+                <input type="text" name="nome" placeholder="Digite seu nome completo"><br>
 
-                <label for="email">Email:</label>
-                <input type="email" name="email" placeholder="Digite seu e-mail">
+                <label for="email">Email:</label><br>
+                <input type="email" name="email" placeholder="Digite seu e-mail"><br>
 
-                <label for="senha">Senha:</label>
-                <input type="password" name="senha" placeholder="Digite sua senha">
+                <label for="senha">Senha:</label><br>
+                <input type="password" name="senha" placeholder="Digite sua senha"><br>
 
                 <div class="alertError"><?php echo $this->erro['registro'];?></div>
 
