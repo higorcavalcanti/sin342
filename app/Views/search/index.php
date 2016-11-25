398971ef103a832ee1b51131fd25cc64767be9aa
Higor Daniel <higordaniell@live.com>
2016-11-25 19:41:48 -0200

    Search

--- app/Views/search/index.php
+++ app/Views/search/index.php
@@ -1,13 +1,13 @@
 <h1><?= $this->title ?></h1>
 <div id="produtos">
     <ul><?php foreach($this->livros as $livro): ?>
-        <li>
-            <div class="boxProduto">
-                <div class="image"><?= $livro->imageLink(true); ?></div>
-                <div> <?= $livro->getTitulo(); ?></div>
-                <div> <?= $livro->getAutor(); ?> </div>
-            </div>
-        </li>
-    <?php endforeach; ?></ul>
+            <li>
+                <div class="boxProduto">
+                    <p id="image"><?= $livro->imageLink(true); ?></p>
+                    <p class="tituloProduto"> <?= $livro->getTitulo(); ?></p>
+                    <p class="autorProdutos"> <?= $livro->getAutor(); ?> </p>
+                </div>
+            </li>
+        <?php endforeach; ?></ul>
 </div>
 <div style="clear:both"></div>
