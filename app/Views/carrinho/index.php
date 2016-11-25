49e95ea6a82101e580de72b376afe8f247b2ca63
Higor Daniel <higordaniell@live.com>
2016-11-24 22:23:52 -0200

    Carrinho

--- app/Views/carrinho/index.php
+++ app/Views/carrinho/index.php
@@ -1,43 +1,44 @@
 <div id="conteudo">
     <h1>Carrinho</h1>
-    <?php if(empty($this->carrinho)):?>
+    <?php if (empty($this->carrinho)): ?>
         <div class="vazioCarrinho">
             Nenhum produto no carrinho.
         </div>
 
-        <?php else:?>
-            <div id="carrinhoTabelas">
-                <table style="width: 900px; margin: auto; text-align: center" >
-                    <thead>
-                        <th width="30%">Imagem</th>
-                        <th width="30%">Produto</th>
-                        <th width="30%">Quantidade</th>
-                        <th width="30%">Remover</th>
-                    </thead>
-                    <tbody>
+    <?php else: ?>
+    <div id="carrinhoTabelas">
+        <table style="width: 900px; margin: auto; text-align: center">
+            <thead>
+            <th width="30%">Imagem</th>
+            <th width="30%">Produto</th>
+            <th width="30%">Quantidade</th>
+            <th width="30%">Remover</th>
+            </thead>
+            <tbody>
 
-                    <!--Colocar linha de baixo do titulo-->
-                    <td><hr></td>
-                    <td><hr></td>
-                    <td><hr></td>
-                    <td><hr></td>
+            <!--Colocar linha de baixo do titulo-->
+            <td><hr></td>
+            <td><hr></td>
+            <td><hr></td>
+            <td><hr></td>
 
-                    <?php foreach($this->carrinho as $item): ?>
-                        <tr>
-                            <td><?=$item['livro']->imageLink(true);?></td>
-                            <td><?= $item['livro']->getTitulo(); ?></td>
-                            <td><?= $item['quantidade']?></td>
-                            <td> <a href="carrinho/remove/<?=$item['livro']->getId()?>"> Remover </a></td>
-                        </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        <br>
-
-    <button class="carrinhoInput" type="button" value="teste">Finalizar</button>
+            <?php foreach ($this->carrinho as $item): ?>
+                <tr>
+                    <td><?= $item['livro']->imageLink(true); ?></td>
+                    <td><?= $item['livro']->getTitulo(); ?></td>
+                    <td><?= $item['quantidade'] ?></td>
+                    <td><a href="carrinho/remove/<?= $item['livro']->getId() ?>"> Remover </a></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <br>
+    <div class="right" style="padding-right: 40px">
+        <button class="carrinhoInput" type="button" value="teste">Finalizar</button>
+    </div>
 </div>
 
-    <?php endif;?>
+<?php endif; ?>
 
 </div>
