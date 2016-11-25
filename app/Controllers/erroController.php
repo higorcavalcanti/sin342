9c9fda6a84bfb8be5fe63253459ea0673aa392b5
Higor Daniel <higordaniell@live.com>
2016-11-25 19:42:11 -0200

    Header nos erros

--- app/Controllers/erroController.php
+++ app/Controllers/erroController.php
@@ -6,10 +6,12 @@ class erroController extends Controller {
     }
 
 	public function e401(){
+        header('HTTP/1.0 401 Unauthorized');
         $this->_page->view('erro/401');
 	}
 
     public function e404(){
+        header("HTTP/1.0 404 Not Found");
         $this->_page->view('erro/404');
     }
 }
