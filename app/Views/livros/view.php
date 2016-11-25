1b8d797215e09055e29a795b6905383e621160d5
Daniel Araujo <danielaraujos@live.com>
2016-11-25 13:33:46 -0200

    Arrumando tela de Descricao

--- app/Views/livros/view.php
+++ app/Views/livros/view.php
@@ -1,16 +1,30 @@
 <div id="conteudo">
-    <h1><?= $this->livro->getTitulo(); ?></h1>
 
-    <?= $this->livro->image(); ?>
+    <div id="blocoTotal">
+        <div class="boxTitulo">
+            <?= $this->livro->getTitulo(); ?>
+        </div>
+       <div class="blocoImagem">
+           <?= $this->livro->image(); ?>
+       </div>
 
-    <br>
+        <div class="blocoConteudo">
+            <b>Autor:</b> <?= $this->livro->getAutor(); ?> <br>
+            <b>Categoria: </b><?= $this->livro->getCategoria()->getNome(); ?> <br>
+            <b>Editora:</b> <?= $this->livro->getEditora()->getNome(); ?> <br>
+            <b>Ano: </b>2000 <br>
+            <b>Descrição: </b>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker. <br>
+        </div>
 
-    Titulo: <?= $this->livro->getTitulo(); ?> <br>
-    Autor: <?= $this->livro->getAutor(); ?> <br>
-    Categoria: <?= $this->livro->getCategoria()->getNome(); ?> <br>
-    Editora: <?= $this->livro->getEditora()->getNome(); ?> <br>
+        <div class="blocoValor">
+            <div class="blocoPreco">R$ 300,00</div>
+            <a href="carrinho/add/<?= $this->livro->getId();?>">
+                <button class="blocoButton" type="button" value="add">Adicionar ao carrinho</button>
+            </a>
+        </div>
 
-    <br><br><br>
 
-    <a href="carrinho/add/<?= $this->livro->getId();?>"> [BOTAO: Adicionar ao carrinho] </a>
+
+    </div>
+
 </div>
